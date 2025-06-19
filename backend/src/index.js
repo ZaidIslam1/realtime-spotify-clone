613380ec1d26f32633507d773d9d8fd6417678f1
ZaidIslam1 <121275717+ZaidIslam1@users.noreply.github.com>
2025-06-19 00:16:42 -0600

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -63,17 +63,17 @@ cron.schedule("0 * * * *", () => {
 	}
 });
 
-app.use("/api/users", userRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/songs", songRoutes);
-app.use("/api/albums", albumRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/admin", adminRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/songs", songRoutes);
+// app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html")); 
 	});
 }
 
