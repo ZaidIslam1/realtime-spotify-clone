1f7e6c400480cb4be60b0b82521572f4df34978d
ZaidIslam1 <121275717+ZaidIslam1@users.noreply.github.com>
2025-06-26 20:08:34 -0600

    No need to refresh AddAlbumDialog

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -7,26 +7,28 @@ import { buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
 
 const Topbar = () => {
-    const {isAdmin} = useAuthStore();
+    const { isAdmin } = useAuthStore();
     return (
         <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10 rounded-md">
             <div className="flex gap-2 items-center font-semibold">
-                <img src="/spotify.png" className="size-8" alt="Spotify logo" />
-                Spotify
+                <Link to="/" className="rounded-lg">
+                    <img src="/spotify.png" className="size-8" alt="Spotify logo" />
+                    Spotify
+                </Link>
             </div>
             <div className="flex items-center gap-4">
                 {isAdmin && (
-                    <Link to={"/admin"} className={cn(buttonVariants({variant: "outline"}))}>
-                        <LayoutDashboardIcon className="size-4 mr-2"/>
+                    <Link to={"/admin"} className={cn(buttonVariants({ variant: "outline" }))}>
+                        <LayoutDashboardIcon className="size-4 mr-2" />
                         Admin Dashboard
                     </Link>
                 )}
 
                 <SignedOut>
-                    <SignInOAuthButtons/>
+                    <SignInOAuthButtons />
                 </SignedOut>
 
-                <UserButton/>
+                <UserButton />
             </div>
         </div>
 
